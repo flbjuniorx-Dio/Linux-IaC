5bca69549b45f52b00d6a6b3f0bfdc761b5b2fce
flbjuniorx <flbjuniorx@gmail.com>
2022-08-26 07:34:05 -0300

    Usar home folder (~)

--- iacl.sh
+++ iacl.sh
@@ -6,7 +6,7 @@
 
 echo "Criando diretórios..."
 for FOLDER in ${FOLDERS[@]}; do
-	mkdir /$FOLDER;
+	mkdir ~/$FOLDER;
 	if [$FOLDER = "public"]; then
 		chmod 770 $FOLDER
 	else
@@ -18,11 +18,11 @@ echo "Criando grupos..."
 for GRUPO in ${GRUPOS[@]}; do
 	groupadd $GRUPO;
 	if [$GRUPO = "GRP_ADM"]; then
-		chown root:$GRUPO /adm;
+		chown root:$GRUPO ~/adm;
 	elif [$GRUPO = "GRP_VEN"]; then
-		chown root:$GRUPO /ven;
+		chown root:$GRUPO ~/ven;
 	elif [$GRUPO = "GRP_SEC"]; then
-		chown root:$GRUPO /sec;
+		chown root:$GRUPO ~/sec;
 	fi
 
 done
@@ -31,11 +31,11 @@ echo "Criando usuários..."
 for USER in ${USERS[@]}; do 
     useradd $USER -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
     if [($USER = "carlos") OR ($USER = "maria") OR ($USER = "joao")]; then
-	    echo "usermod -G GRP_ADM $USER"
+	    usermod -G GRP_ADM $USER
     elif [($USER = "debora") OR ($USER = "sebastiana") OR ($USER = "roberto")]; then
-	    echo "usermod -G GRP_VEN $USER"
+	    usermod -G GRP_VEN $USER
     elif [($USER = "josefina") OR ($USER = "amanda") OR ($USER = "rogerio")]; then
-	    echo "usermod -G GRP_SEC $USER"
+	    usermod -G GRP_SEC $USER
     fi
 done
 
